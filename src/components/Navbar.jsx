8c8c6e801cc9accc869a0068cf9400b98d190050
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2023-11-14 16:09:30 +0100

    Changed logo.svg src in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 flex justify-between items-center px-3 py-4 shadow-md bg-[#333] max-h-68">
       <Link to="/" className="text-xl cursor-pointer text-violet-700 font-bold">
-        <img src="assets/logo.svg" width={100} height={20} />
+        <img src="../assets/logo.svg" width={100} height={20} />
       </Link>
       <ul className="flex gap-5 text-lg">
         <li>
